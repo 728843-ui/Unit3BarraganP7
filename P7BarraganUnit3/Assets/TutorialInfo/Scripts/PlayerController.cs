4f26d77e5748209588d630a19132f662069af803
728843-ui <728843@student.dusd.net>
2026-03-26 17:13:39 -0700

    guibvcdsbvc;kjdvb

--- P7BarraganUnit3/Assets/TutorialInfo/Scripts/PlayerController.cs
+++ P7BarraganUnit3/Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    // Start is called once before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            isOnGround = false;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
+
